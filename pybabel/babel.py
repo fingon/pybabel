9affb6344710d10cebe99968b0723b3050af47cd
Markus Stenberg <markus.stenberg@iki.fi>
2015-03-25 10:41:35 -0500

    Finished first iteration of implementing the protocol. Not tested yet (duh).

--- pybabel/babel.py
+++ pybabel/babel.py
@@ -9,8 +9,8 @@
 # Copyright (c) 2015 Markus Stenberg
 #
 # Created:       Wed Mar 25 03:48:40 2015 mstenber
-# Last modified: Wed Mar 25 08:11:13 2015 mstenber
-# Edit time:     151 min
+# Last modified: Wed Mar 25 10:38:18 2015 mstenber
+# Edit time:     200 min
 #
 """
 
@@ -30,6 +30,7 @@ IHU_HOLD_TIME_MULTIPLIER = 3.5
 ROUTE_EXPIRY_TIME_MULTIPLIER = 3.5
 SOURCE_GC_TIME = 3 * 60
 URGENT_JITTER = 0.2
+HOP_COUNT = 16
 
 RECENT_HELLO_TIME = 60 # TBD - not clear in RFC!
 
@@ -39,7 +40,7 @@ class TLVQueuer:
     t = None
     q = None
     timeout = None
-    def queue_tlv(self, j, tlv):
+    def queue_tlv(self, tlv, j=HELLO_INTERVAL / 2):
         if self.q is None: self.q = []
         sys = self.get_sys()
         now = sys.time()
@@ -86,6 +87,16 @@ class BabelNeighbor(TLVQueuer):
         self.last_hello = self.get_sys().time()
         self.i.b.route_selection()
     def process_ihu(self, tlv):
+        if tlv.ae != 0:
+            # If it has address set, and it is not us, ignore
+            # (~implicit based on reading of 4.4.6)
+            try:
+                ip = tlv_to_ip(tlv)
+            except:
+                # 4.4.6 - invalid IHU AE => ignore
+                return
+            if ip != self.i.ip:
+                return
         if self.ihu_timer:
             self.ihu_timer.cancel()
         self.update_transmission_cost(tlv.rxcost)
@@ -115,9 +126,13 @@ class BabelNeighbor(TLVQueuer):
         if rxcost == INF:
             return
         ihu = IHU(interval=_t2b(HELLO_INTERVAL),
-                  rxcost=rxcost, **_ll2tlv(self.ip))
-        self.queue_tlv(HELLO_INTERVAL/2, ihu)
-    def process_update(self, tlv, rid, prefix):
+                  rxcost=rxcost, **ll_to_tlv_args(self.ip))
+        self.i.queue_tlv(ihu)
+    def process_update_all(self, tlv, rid, nh):
+        assert tlv.metric == INF
+        for prefix in self.routes.keys():
+            self.process_update(tlv, rid, prefix, nh)
+    def process_update(self, tlv, rid, prefix, nh):
         sk = (prefix, rid)
         rk = prefix
         metric = tlv.metric
@@ -163,13 +178,6 @@ class BabelNeighbor(TLVQueuer):
                                              rk)
         self.i.b.route_selection()
 
-def _ll2tlv(ip):
-    s = ip.exploded.replace(':', '')
-    assert len(s) == 32
-    s = s[16:] # skip first 8 bytes
-    b = binascii.unhexlify(s)
-    return {'ae': 3, 'body': b}
-
 
 def _b2t(v):
     return v / 100.0
@@ -178,10 +186,10 @@ def _t2b(v):
     return int(v * 100)
 
 class BabelInterface(TLVQueuer):
-    def __init__(self, b, ifname):
+    def __init__(self, b, ifname, ip):
         self.b = b
         self.ifname = ifname
-
+        self.ip = ip
         self.neighs = {}
 
         # per-if hello seqno
@@ -189,8 +197,11 @@ class BabelInterface(TLVQueuer):
 
         # TBD update timer
 
-        # Start by sending hello immediately
+        # Queue hello immediately
         self.hello_timer()
+
+        # And wildcard update request
+        self.queue_tlv(UpdateReq(ae=0, plen=0))
     def get_sys(self):
         return self.b.sys
     def hello_timer(self):
@@ -202,19 +213,49 @@ class BabelInterface(TLVQueuer):
             self.neighs[ip] = BabelNeighbor(self, ip)
         return self.neighs[ip]
     def process_tlvs(self, address, tlvs):
+        rid = None
+        nh = None
+        default_prefix = {}
         for tlv in tlvs:
             if isinstance(tlv, AckReq):
-                self.neighbor(address).queue_tlv(_b2t(tlv.interval),
-                                                 Ack(nonce=tlv.nonce))
+                self.neighbor(address).queue_tlv(Ack(nonce=tlv.nonce),
+                                                 _b2t(tlv.interval))
             elif isinstance(tlv, Hello):
                 self.neighbor(address).process_hello(tlv)
             elif isinstance(tlv, IHU):
                 self.neighbor(address).process_ihu(tlv)
             elif isinstance(tlv, Update):
-                self.neighbor(address).process_update(tlv, rid, prefix)
+                if tlv.flags & 0x80:
+                    default_prefix[tlv.ae] = tlv.body
+                if tlv.flags & 0x40:
+                    rid = tlv.body[-8:]
+                if tlv.omitted:
+                    d = {'ae': tlv.ae, 'body': default_prefix.get(ae, b'')[:tlv.omitted] + tlv.body}
+                else:
+                    d = tlv
+                if tlv.ae:
+                    # MUST ignore invalid AE
+                    try:
+                        prefix = tlv_to_prefix(d)
+                    except:
+                        continue
+                    self.neighbor(address).process_update(tlv, rid, prefix, nh)
+                else:
+                    self.neighbor(address).process_update_all(tlv, rid, nh)
+            elif isinstance(tlv, RouteReq):
+                self.b.process_route_req_i(self, tlv)
+            elif isinstance(tlv, SeqnoReq):
+                self.b.process_seqno_req_n(self.neighbor(address), tlv)
+            elif isinstance(tlv, RID):
+                rid = tlv.rid
+            elif isinstance(tlv, NH):
+                # Unknown ones MUST be silently ignored
+                try:
+                    nh = tlv_to_ip_or_ll(tlv)
+                except:
+                    pass
     def queue_hello(self):
-        self.queue_tlv(HELLO_INTERVAL/2,
-                       Hello(seqno=self.seqno,
+        self.queue_tlv(Hello(seqno=self.seqno,
                              interval=_t2b(self.b.hello_interval)))
         # Queue also IHUs for every neighbor
         for ip, n in self.neighs():
@@ -244,14 +285,16 @@ class Babel:
 
         self.selected_routes = {}
         # [prefix] = {'metric', 'n', 'r'}
-    def interface(self, name):
-        if name not in self.ifs:
-            self.ifs[name] = BabelInterface(self, name)
-        return self.ifs[name]
 
+        self.update_timer()
+    def interface(self, name, ip=None):
+        if name not in self.ifs:
+            if not ip: ip = self.sys.get_if_ip(name)
+            assert ip
+            self.ifs[name] = BabelInterface(self, name, ip)
+        return self.ifs[name]
     def process_inbound(self, ifname, address, p):
         self.interface(ifname).process_tlvs(address, Packet.decode(p))
-
     def route_selection(self):
         # 3.6
         sr = {}
@@ -274,10 +317,16 @@ class Babel:
             d2 = self.selected_routes[prefix]
             # MUST send update in timely manner if rid changes
             if d2['r']['rid'] != d['r']['rid']:
-                self.queue_update(URGENT_JITTER, prefix, d)
+                self.queue_update(prefix, d, URGENT_JITTER)
             # SHOULD send if route redacted
             if d2['r']['metric'] < INF and d['r']['metric'] == INF:
-                self.queue_update(URGENT_JITTER, prefix, d)
+                self.queue_update(prefix, d, URGENT_JITTER)
+                # 3.8.2.1 MUST send seqno request (no feasible routes)
+                tlv = SeqnoReq(seqno=d['r']['seqno'] + 1,
+                               hopcount=HOP_COUNT,
+                               rid=d['r']['rid'],
+                               **prefix_to_tlv_args(prefix))
+                self.queue_tlv(tlv)
     def maintain_feasibility(self, d):
         # 3.7.3 maintain feasibility distance
         if d['metric'] == INF:
@@ -301,6 +350,70 @@ class Babel:
         sd['timer'] = self.sys.call_later(SOURCE_GC_TIME, self.source_gc_timer, sk)
     def source_gc_timer(self, sk):
         del self.sources[sk]
-    def queue_update(self, j, prefix, d):
+    def update_timer(self):
+        # 3.7.1
+        for prefix, d in self.selected_routes.items():
+            self.queue_update(prefix, d)
+        self.sys.call_later(UPDATE_INTERVAL, self.update_timer)
+    def queue_tlv(self, tlv, *a):
+        for i in self.ifs.values():
+            i.queue_tlv(tlv, *a)
+    def queue_update(self, prefix, d, *a):
         self.maintain_feasibility(d)
-        xxx
+        u = self.to_update_tlv(prefix, d)
+        self.queue_tlv(u, *a)
+    def to_update_tlv(self, prefix, d):
+        flags = 0
+        omitted = 0
+        interval = UPDATE_INTERVAL
+        u = Update(flags=flags, omitted=omitted, interval=_t2b(interval), seqno=d['seqno'], metric=d['metric'])
+        prefix_to_tlv_args(prefix, u)
+        return u
+    def process_route_req_i(self, i, tlv):
+        # 3.8.1.1
+        if tlv.ae == 0:
+            # SHOULD send full routing table dump
+            for prefix, d in self.selected_routes.items():
+                u = self.to_update_tlv(prefix, d)
+                i.queue_tlv(u)
+            return
+        # MUST send an update to individual req.
+        prefix = tlv_to_prefix(tlv)
+        d = self.selected_routes.get(prefix)
+        d = d or {'metric': INF, 'seqno': 0}
+        u = self.to_update_tlv(prefix, d)
+        i.queue_tlv(u)
+    def process_seqno_req_n(self, n, tlv):
+        i = n.i
+        # 3.8.1.2
+        prefix = tlv_to_prefix(tlv)
+        d = self.selected_routes.get(prefix)
+        if d is None: return # not present, ignored
+        if d['metric'] == INF: return # infinite metric
+        if tlv.rid != d['rid'] or tlv.seqno <= d['seqno']:
+            # MUST send update if prefix varies or
+            # ' no smaller'
+            u = self.to_update_tlv(prefix, d)
+            i.queue_tlv(u)
+            return
+        if tlv.rid == d['rid'] and tlv.seqno > d['seqno']:
+            if tlv.rid == self.rid:
+                self.seqno += 1
+                d['seqno'] += 1
+                u = self.to_update_tlv(prefix, d)
+                i.queue_tlv(u)
+                return
+            if tlv.hopcount >= 2:
+                for i2 in self.ifs.values():
+                    for n2 in i2.neighs.values():
+                        r2 = n2.routes.get(prefix)
+                        if not r2: continue
+                        if r2['metric'] == INF: continue
+                        if n2.get_cost() == INF: continue
+                        if n2 == n: continue
+                        tlv2 = SeqnoReq(seqno=tlv.seqno,
+                                        hopcount=tlv.hopcount-1,
+                                        rid=tlv.rid,
+                                        **prefix_to_tlv_args(prefix))
+                        n2.queue_tlv(tlv2, URGENT_JITTER)
+                return
